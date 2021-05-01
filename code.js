3536f3abca70b2ad6efed13fbdb365b1a775471e
Aurange <auragar@users.noreply.github.com>
2021-05-01 18:00:31 -0500

    Create code.js

---
+++ code.js
@@ -0,0 +1,17 @@
+//Created by Aurange
+
+Date.prototype.isLeapYear = function(){
+  return x;
+}
+
+Date.prototype.getDayOfYear = function(){
+  return x;
+}
+
+function dayOfYearToDateString(a){
+  return x;
+}
+
+function seriesFinaleDate(a, b){
+  return x;
+}
