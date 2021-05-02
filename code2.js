06e6fd392e0aa1921c43bdda395d153f79bd8fb4
Aurange <auragar@users.noreply.github.com>
2021-05-01 19:40:39 -0500

    Create code2.js

---
+++ code2.js
@@ -0,0 +1,9 @@
+function performAction(){
+  var startDate = document.getElementById("input").value;
+  if(startDate.length > 0){
+    startDate = startDate.split("-");
+    startDate = new Date(startDate[0] + ", " + parseInt(startDate[1]) + ", " + startDate[2]);
+    var episodeCount = document.getElementById("input2").value;
+    document.getElementById("output").innerText = " ends on " + seriesFinaleDate(startDate.getDayOfYear(), episodeCount) + ".";
+  }
+}
