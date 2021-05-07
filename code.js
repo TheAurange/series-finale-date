6021578e41a837daeb80e455d2c9668673c06800
Aurange <auragar@users.noreply.github.com>
2021-05-06 21:06:29 -0500

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 //Created by Aurange
 
-Date.prototype.isLeapYear = function() {
+/* Date.prototype.isLeapYear = function() {
   var curYr = this.getFullYear();
   if((curYr & 3) != 0) return false;
   return ((curYr % 100) != 0 || (curYr % 400) == 0);
@@ -13,22 +13,4 @@ Date.prototype.getDayOfYear = function() {
   var curDayOfYear = monsToDays[curMon] + curDay;
   if(curMon > 1 && this.isLeapYear()) curDayOfYear++;
   return curDayOfYear;
-};
-
-function dayOfYearToDateString(dayOfYear){
-  var curDate = new Date();
-  var curYr = curDate.getFullYear();
-  var monsToDays = [0, 31, 59, 90, 120, 151, 181, 212, 243, 273, 304, 334];
-  var mon;
-  for(var i = 0; i < monsToDays.length; i++){
-    if(monsToDays[i] >= dayOfYear){
-      mon = i - 1;
-      break;
-    }
-  }
-  return (mon + 1) + "/" + (dayOfYear - monsToDays[mon]) + "/" + curYr;
-}
-
-function seriesFinaleDate(startDateAsDayOfYear, numOfEps){
-  return dayOfYearToDateString(startDateAsDayOfYear + ((numOfEps - 1) * 7));
-}
+}; */
