18bd7590b8f9e587521e665bad240205cc4caf48
Aurange <auragar@users.noreply.github.com>
2021-05-06 21:26:55 -0500

    Update code.js

--- code.js
+++ code.js
@@ -2,11 +2,12 @@
 
 function performAction(){
   var startDate = document.getElementById("input").value;
-  if(startDate.length > 0){
+  var episodeCount = document.getElementById("input2").value;
+  if(startDate.length > 0 && episodeCount.length > 0){
     startDate = startDate.split("-");
     startDate = new Date(startDate[0] + ", " + parseInt(startDate[1]) + ", " + startDate[2]);
-    var episodeCount = document.getElementById("input2").value;
-    document.getElementById("output").innerText = " ends on " + startDate.setDate(startDate.getDate() + ((episodeCount - 1) * 7)); + ".";
+    startDate.setDate(startDate.getDate() + ((episodeCount - 1) * 7));
+    document.getElementById("output").innerText = " ends on " + (startDate.getMonth() + 1) + "/" + startDate.getDate() + "/" + startDate.getFullYear() + ".";
   }
   this.blur();
 }
