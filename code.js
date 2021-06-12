d323e2b2a7a7206173bc58173b9158e0aac1aef9
Aurange <auragar@users.noreply.github.com>
2021-06-12 03:31:31 -0500

    Update code.js

--- code.js
+++ code.js
@@ -5,11 +5,15 @@
 function performAction(inputElem){
   let startDate = document.getElementById("input").value;
   let episodeCount = document.getElementById("input2").value;
+
   if(startDate.length > 0 && episodeCount.length > 0){
     startDate = startDate.split("-");
     startDate = new Date(startDate[0] + ", " + startDate[1] + ", " + startDate[2]);
     startDate.setDate(startDate.getDate() + ((episodeCount - 1) * 7));
-    document.getElementById("output").innerText = " ends on " + (startDate.getMonth() + 1) + "/" + startDate.getDate() + "/" + startDate.getFullYear() + " (" + (Math.round((new Date() - startDate) / 86400000) >= 0) ? "Ended" : Math.round(Math.abs(new Date() - startDate) / 86400000) + " Days Remaining" + ").";
+
+    let remaining = (Math.round((new Date() - startDate) / 86400000) >= 0) ? "Ended" : Math.round(Math.abs(new Date() - startDate) / 86400000) + " Days Remaining";
+
+    document.getElementById("output").innerText = " ends on " + (startDate.getMonth() + 1) + "/" + startDate.getDate() + "/" + startDate.getFullYear() + " (" + remaining + ").";
   }
   inputElem.blur();
 }
