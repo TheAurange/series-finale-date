2941815cf9836b29246e34b7004a41ce573cd78b
Aurange <auragar@users.noreply.github.com>
2021-08-17 22:24:15 -0500

    Update code.js

--- code.js
+++ code.js
@@ -8,7 +8,7 @@ function performAction(inputElem){
   if(startDate.length > 0 && episodeCount.length > 0 && delayCount.length > 0){
     startDate = startDate.split("-");
     startDate = new Date(startDate[0] + ", " + startDate[1] + ", " + startDate[2]);
-    startDate.setDate(startDate.getDate() + (((episodeCount - 1) + delayCount) * 7));
+    startDate.setDate(startDate.getDate() + (((parseInt(episodeCount) - 1) + parseInt(delayCount)) * 7));
 
     if(Math.round((new Date() - startDate) / 86400000) >= 0) remaining = "Ended";
     else if(Math.round(Math.abs(new Date() - startDate) / 604800000) > 1) remaining = Math.round(Math.abs(new Date() - startDate) / 604800000) + " Episodes Remaining";
