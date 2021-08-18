3afbe2cf7274a4ca6a1b7f577a9400fdd23d4ba7
Aurange <auragar@users.noreply.github.com>
2021-08-17 22:07:24 -0500

    Update code.js

--- code.js
+++ code.js
@@ -3,12 +3,12 @@
 "use strict";
 
 function performAction(inputElem){
-  let startDate = document.getElementById("input").value, episodeCount = document.getElementById("input2").value, remaining;
+  let startDate = document.getElementById("input").value, episodeCount = document.getElementById("input2").value, delayCount = document.getElementById("input3").value, remaining;
 
-  if(startDate.length > 0 && episodeCount.length > 0){
+  if(startDate.length > 0 && episodeCount.length > 0 && delayCount.length > 0){
     startDate = startDate.split("-");
     startDate = new Date(startDate[0] + ", " + startDate[1] + ", " + startDate[2]);
-    startDate.setDate(startDate.getDate() + ((episodeCount - 1) * 7));
+    startDate.setDate(startDate.getDate() + (((episodeCount - 1) + delayCount) * 7));
 
     if(Math.round((new Date() - startDate) / 86400000) >= 0) remaining = "Ended";
     else if(Math.round(Math.abs(new Date() - startDate) / 604800000) > 1) remaining = Math.round(Math.abs(new Date() - startDate) / 604800000) + " Episodes Remaining";
