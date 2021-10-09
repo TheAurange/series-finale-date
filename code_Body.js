e2d5c68ce42bc0b2c4014e5cec60b79e405c3934
Aurange <auragar@users.noreply.github.com>
2021-10-08 20:28:55 -0500

    Update code_Body.js

--- code_Body.js
+++ code_Body.js
@@ -10,8 +10,8 @@ function performAction(inputElem){
     startDate = new Date(startDate[0] + ", " + startDate[1] + ", " + startDate[2]);
     startDate.setDate(startDate.getDate() + (((parseInt(episodeCount) - 1) + parseInt(delayCount)) * 7));
 
-    if(Math.round((new Date() - startDate) / 86400000) >= 0) remaining = "Ended";
-    else if(Math.round(Math.abs(new Date() - startDate) / 604800000) > 1) remaining = Math.round(Math.abs(new Date() - startDate) / 604800000) + " Episodes Remaining";
+    if((new Date() - startDate) / 86400000 >= 0) remaining = "Ended";
+    else if(Math.abs(new Date() - startDate) / 604800000 > 1) remaining = Math.round(Math.abs(new Date() - startDate) / 604800000) + " Episodes Remaining";
     else remaining = "1 Episode Remaining";
 
     document.getElementById("output").innerText = " ends on " + (startDate.getMonth() + 1) + "/" + startDate.getDate() + "/" + startDate.getFullYear() + ". (" + remaining + ")";
