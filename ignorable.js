e05f0debf83d502df10656d75c1ef9e5011e4b0e
Aurange <auragar@users.noreply.github.com>
2022-05-05 23:45:04 -0500

    Update ignorable.js

--- ignorable.js
+++ ignorable.js
@@ -3,6 +3,7 @@
 "use strict";
 
 if(window.location !== window.parent.location){
+  document.body.style.backgroundColor = "#253829";
   document.querySelectorAll("body > *")[0].style.display = "none";
   document.querySelectorAll("body > *")[1].style.display = "none";
 }
