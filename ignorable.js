ecb0009232a28949efa0bac239df6d1ecaf4864f
Aurange <auragar@users.noreply.github.com>
2022-05-05 23:56:31 -0500

    Update ignorable.js

--- ignorable.js
+++ ignorable.js
@@ -5,6 +5,7 @@
 if(window.location !== window.parent.location){
   document.body.style.backgroundColor = "#253829";
   document.body.style.color = "#FFFFFF";
+  document.body.style.fontFamily = "Sans-Serif";
   document.querySelectorAll("body > *")[0].style.display = "none";
   document.querySelectorAll("body > *")[1].style.display = "none";
 }
